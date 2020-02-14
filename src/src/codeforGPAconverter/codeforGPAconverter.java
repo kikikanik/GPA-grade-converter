5a06b1d83c7da8efee3111be0f9a53d9f193ec6a
kikikanik <55113869+kikikanik@users.noreply.github.com>
2020-02-13 20:55:50 -0500

    real Version 1 with variable for numerical grade

--- src/src/codeforGPAconverter/codeforGPAconverter.java
+++ src/src/codeforGPAconverter/codeforGPAconverter.java
@@ -5,58 +5,74 @@ public class codeforGPAconverter {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Please Enter your letter grade:" );
-		String letterGrade= in.next(); 
+		double numberGrade= 0.0;
+		double plusConvertion = numberGrade + 0.3;
+		double minusConvertion = numberGrade - 0.3;
+
+		int exitPlease = 1; 
+		while(exitPlease != 0) {
+
+			System.out.print("Please Enter your letter grade:" );
+			String letterGrade= in.next(); 
 			switch (letterGrade) {
 			case "A+": 
-				System.out.println("4.3");
+				numberGrade = 4;
+				System.out.println(numberGrade+plusConvertion);
 				break;
 			case "A":
-				System.out.println("4");
+				numberGrade = 4;
+				System.out.println(numberGrade);
 				break;
 			case "A-":
-				System.out.println("3.7");
+				numberGrade = 4;
+				System.out.println(numberGrade+minusConvertion);
 				break;
 			case "B+":
-				System.out.println("3.3");
+				numberGrade = 3;
+				System.out.println(numberGrade+plusConvertion);
 				break;
 			case "B":
-				System.out.println("3");
+				numberGrade = 3;
+				System.out.println(numberGrade);
 				break;
 			case "B-":
-				System.out.println("2.7");
+				numberGrade = 3;
+				System.out.println(numberGrade+minusConvertion);
 				break;
 			case "C+":
-				System.out.println("2.3");
+				numberGrade = 2;
+				System.out.println(numberGrade+plusConvertion);
 				break;
 			case "C":
-				System.out.println("2");
+				numberGrade = 2;
 				break;
 			case "C-":
-				System.out.println("1.7");
+				numberGrade = 2;
+				System.out.println(numberGrade+minusConvertion);
 				break;
 			case "D+":
-				System.out.println("1.3");
+				numberGrade = 1;
+				System.out.println(numberGrade+plusConvertion);
 				break;
 			case "D":
-				System.out.println("1");
+				numberGrade = 1;
 				break;
 			case "D-":
-				System.out.println(".7");
+				numberGrade = 1;
+				System.out.println(numberGrade+minusConvertion);
 				break;
 			case "F":
 				System.out.println("0");
 				break;
 			case "Q":
-				System.out.println("Closing Program.");
-				java.lang.System.exit(0);
+				System.out.print("Program Closed.");
+				exitPlease = 0;
 				break;
 			default: 
 				System.out.println("Incorrect Input");
 				break;
 			}
-		
+		}
 		in.close();
 	}
-
 }
